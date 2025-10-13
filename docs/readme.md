3a3b7d13e4a9398c005234c518cec5aff34ac928
Einst <einst1988@gmail.com>
2025-10-13 20:53:07 +0800

    同步原作者代码，增加新英雄装备

---
+++ docs/readme.md
@@ -0,0 +1,2 @@
+git remote add upstream https://github.com/NinjaInShade/clash-armies.git
+git fetch upstream
--- src/lib/server/migration/v0_5_0.ts
+++ src/lib/server/migration/v0_5_0.ts
@@ -2,6 +2,7 @@ import type { MySQL, MigrationFn } from '@ninjalib/sql';
 import type { Unit, Pet } from '$types';
 
 // prettier-ignore
+//这里的53-55是我增加的，56对应原作者的53，这里可以考虑下次将数据库中的版本号改为53，然后把53-56合并为一条53
 export default function migration(runStep: MigrationFn) {
     runStep(47, `
         ALTER TABLE users
@@ -106,4 +107,82 @@ export default function migration(runStep: MigrationFn) {
 		INSERT INTO metrics (name, weight, minAgeHours) VALUES
 			('vote', 50, 1)
 	`);
+	
+
+	runStep(53, `
+		CREATE TABLE  user_refresh_tokens (
+			userId INT NOT NULL,
+			tokenVersion INT NOT NULL DEFAULT 0,
+			lastJti VARCHAR(128) NULL,
+			createdAt TIMESTAMP DEFAULT NOW(),
+            updatedAt TIMESTAMP DEFAULT NOW() ON UPDATE NOW(),
+			PRIMARY KEY (userId),
+			CONSTRAINT fk_user_refresh_tokens_users FOREIGN KEY (userId) REFERENCES users(id)
+			ON DELETE CASCADE
+			ON UPDATE CASCADE
+		);
+	`);
+
+	runStep(54, `
+        ALTER TABLE users
+        ADD COLUMN appleId VARCHAR(255) NULL,
+        ADD COLUMN appleEmail VARCHAR(255) NULL
+    `);
+    runStep(55, `
+        ALTER TABLE users
+        MODIFY COLUMN googleId VARCHAR(255) NULL
+    `);
+
+	runStep(56, async (db: MySQL) => {
+		// Insert data for new "Heroic Torch" equipment
+		const equipmentId = await db.insertOne('equipment', {
+			hero: 'Grand Warden',
+			name: 'Heroic Torch',
+			epic: 1,
+            clashId: 19,
+		});
+		// Insert levels
+		await db.insertMany('equipment_levels', [
+			{ equipmentId, level: 1, blacksmithLevel: 1 },
+			{ equipmentId, level: 2, blacksmithLevel: 1 },
+			{ equipmentId, level: 3, blacksmithLevel: 1 },
+			{ equipmentId, level: 4, blacksmithLevel: 1 },
+			{ equipmentId, level: 5, blacksmithLevel: 1 },
+			{ equipmentId, level: 6, blacksmithLevel: 1 },
+			{ equipmentId, level: 7, blacksmithLevel: 1 },
+			{ equipmentId, level: 8, blacksmithLevel: 1 },
+			{ equipmentId, level: 9, blacksmithLevel: 1 },
+			{ equipmentId, level: 10, blacksmithLevel: 1 },
+			{ equipmentId, level: 11, blacksmithLevel: 1 },
+			{ equipmentId, level: 12, blacksmithLevel: 1 },
+			{ equipmentId, level: 13, blacksmithLevel: 3 },
+			{ equipmentId, level: 14, blacksmithLevel: 3 },
+			{ equipmentId, level: 15, blacksmithLevel: 3 },
+			{ equipmentId, level: 16, blacksmithLevel: 5 },
+			{ equipmentId, level: 17, blacksmithLevel: 5 },
+			{ equipmentId, level: 18, blacksmithLevel: 5 },
+			{ equipmentId, level: 19, blacksmithLevel: 7 },
+			{ equipmentId, level: 20, blacksmithLevel: 7 },
+			{ equipmentId, level: 21, blacksmithLevel: 7 },
+			{ equipmentId, level: 22, blacksmithLevel: 8 },
+			{ equipmentId, level: 23, blacksmithLevel: 8 },
+			{ equipmentId, level: 24, blacksmithLevel: 8 },
+			{ equipmentId, level: 25, blacksmithLevel: 9 },
+			{ equipmentId, level: 26, blacksmithLevel: 9 },
+			{ equipmentId, level: 27, blacksmithLevel: 9 },
+		]);
+
+		// Insert data for new "Lava Hound" level
+		const lavaHound = await db.getRow<Unit>('units', { name: 'Lava Hound' });
+		const iceHound = await db.getRow<Unit>('units', { name: 'Ice Hound' });
+		await db.insertOne('unit_levels', { unitId: lavaHound.id, level: 7, barrackLevel: 6, laboratoryLevel: 14 });
+		await db.insertOne('unit_levels', { unitId: iceHound.id, level: 7, barrackLevel: 6, laboratoryLevel: 1 });
+
+		// Insert data for new "Ice Block" level
+		const iceBlock = await db.getRow<Unit>('units', { name: 'Ice Block' });
+        await db.insertOne('unit_levels', { unitId: iceBlock.id, level: 5, spellFactoryLevel: 7, laboratoryLevel: 15 });
+	});
+
+	
+
 }
